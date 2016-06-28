4f9b2749559e6402688c13d39e5f13e33a76403d
Daniel Baur <daniel.baur@uni-ulm.de>
2016-06-28 11:16:04 +0200

    Initial commit, imported client examples

---
+++ client-examples/src/main/java/io/github/cloudiator/examples/internal/ApplicationType.java
@@ -0,0 +1,16 @@
+package io.github.cloudiator.examples.internal;
+
+import de.uniulm.omi.cloudiator.colosseum.client.entities.LifecycleComponent;
+
+import java.util.Set;
+
+/**
+ * Created by daniel on 27.06.16.
+ */
+public interface ApplicationType {
+
+    Set<LifecycleComponent> lifecycleComponents();
+
+
+
+}
