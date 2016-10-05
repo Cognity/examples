26ee88640937d7d65500234243c1e90816831a1f
Daniel Baur <daniel.baur@uni-ulm.de>
2016-10-05 16:54:32 +0200

    Enabled instances by default

--- client-examples/src/main/java/io/github/cloudiator/examples/MediawikiExample.java
+++ client-examples/src/main/java/io/github/cloudiator/examples/MediawikiExample.java
@@ -43,7 +43,7 @@ public class MediawikiExample {
 
     private static final LB lb = LB.HAPROXY;
 
-    private static final boolean instances = false;
+    private static final boolean instances = true;
     private static final boolean monitoringEnabled = true;
     private static final boolean cleanup = false;
 
